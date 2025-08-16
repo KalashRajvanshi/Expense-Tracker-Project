20338c457401f562af8924e0af3fde2bf7f6d374
kalashrajvanshi <kalash.raj.0987@gmail.com>
2025-08-16 20:34:21 +1000

    Commit 2

--- src/main/java/com/example/expensetrackerproject/controller/TestController.java
+++ src/main/java/com/example/expensetrackerproject/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String home() {
         return "Hello, Expense Tracker is running!";
     }
