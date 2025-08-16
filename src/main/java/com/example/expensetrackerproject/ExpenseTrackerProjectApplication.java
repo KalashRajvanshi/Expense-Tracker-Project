2acf01b42aaa37aba18b849a3992fe8b24051782
kalashrajvanshi <kalash.raj.0987@gmail.com>
2025-08-16 20:28:32 +1000

    Inital Commit

---
+++ src/main/java/com/example/expensetrackerproject/ExpenseTrackerProjectApplication.java
@@ -0,0 +1,16 @@
+package com.example.expensetrackerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ExpenseTrackerProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ExpenseTrackerProjectApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/expensetrackerproject/ExpenseTrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensetrackerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
